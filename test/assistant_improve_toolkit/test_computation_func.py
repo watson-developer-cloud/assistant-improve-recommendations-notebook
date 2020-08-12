87e1491e6a629e4cc4fe9be2058455e22933a91f
ZheZhang <zhangzhe@us.ibm.com>
2020-08-12 16:01:03 -0400

    Create test_computation_func.py

---
+++ test/assistant_improve_toolkit/test_computation_func.py
@@ -0,0 +1,10 @@
+from assistant_improve_toolkit import intersection
+
+
+def test_intersection():
+    list1 = ['node_1', 'node_2']
+    list2 = ['node_1', 'node_2', 'node_3']
+
+    overlap = intersection(list1, list2)
+
+    assert overlap == ['node_1', 'node_2']
