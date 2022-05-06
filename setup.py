2714d146b7e69669eed3474f3b700b3789084927
Reza Fazeli <rezafazeli@gmail.com>
2022-05-06 11:24:24 -0400

    fix: keep __version__ variable (#147)

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@
 import setuptools
 from os import path
 
+__version__ = '1.3.6'
+
 # read contents of README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as file:
